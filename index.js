d78e4e6ab011b511420801481e4e13c968b3e86a
Scott Corgan <scottcorgan@gmail.com>
2015-09-23 12:39:07 -0700

    use figures.arrowRight for namespaces

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
-var tape = require('tape');
-var isPromise = require('is-promise');
-var merge = require('merge');
+var tape = require('tape')
+var isPromise = require('is-promise')
+var merge = require('merge')
+var figures = require('figures')
 
 function namespace (name, setup) {
 
-  setup = setup || {};
+  setup = setup || {}
 
-  var beforeEach = setup.beforeEach || [];
-  var afterEach = setup.afterEach || [];
-  var tapeFn = tape;
+  var beforeEach = setup.beforeEach || []
+  var afterEach = setup.afterEach || []
+  var tapeFn = tape
 
   var tester = function (name2, fn) {
 
-    var testName = name ? name + ' -> ' + name2 : name2;
+    var testName = name ? name + ' ' + figures.arrowRight + ' ' + name2 : name2
 
     return tapeFn(testName, function (t) {
 
-      var end = t.end.bind(t);
+      var end = t.end.bind(t)
 
       // Call all beforeEach fns and set return as context
       // to pass to test methods
 
       var beforeEachReturns = beforeEach.map(function (be) {
 
-        return be();
-      });
+        return be()
+      })
 
-      t.context = merge.apply(null, beforeEachReturns);
+      t.context = merge.apply(null, beforeEachReturns)
       t.end = function () {
 
         afterEach.forEach(function (ae) {
@@ -34,9 +35,9 @@ function namespace (name, setup) {
           ae({
             context: t.context
           })
-        });
-        end();
-      };
+        })
+        end()
+      }
 
       //
       var ret = fn({
@@ -47,56 +48,56 @@ function namespace (name, setup) {
         notDeepEqual: t.notDeepEqual,
         notEqual: t.notEqual,
         context: t.context
-      });
+      })
 
       // Promise
       if (isPromise(ret)) {
         ret.then(function () {
 
-          return end();
-        });
+          return end()
+        })
       }
       // Thunk
       else if (typeof ret === 'function') {
-        ret(end);
+        ret(end)
       }
       // Call end of synchronous tests automatically
       else {
-        end();
+        end()
       }
-    });
+    })
   }
 
   tester.beforeEach = function (fn) {
 
-    return beforeEach.push(fn);
-  };
+    return beforeEach.push(fn)
+  }
   tester.afterEach = function (fn) {
 
-    return afterEach.push(fn);
-  };
+    return afterEach.push(fn)
+  }
 
   tester.namespace = function (name2) {
 
     return namespace(name + ' -> ' + name2, {
       beforeEach: beforeEach,
       afterEach: afterEach,
-    });
-  };
+    })
+  }
 
   tester.only = function () {
 
-    tapeFn = tape.only;
-    tester.apply(null, arguments);
-    tapeFn = tape;
-  };
+    tapeFn = tape.only
+    tester.apply(null, arguments)
+    tapeFn = tape
+  }
 
-  tester.skip = function () {};
+  tester.skip = function () {}
 
-  return tester;
+  return tester
 }
 
 module.exports = {
   namespace: namespace,
   test: tape
-};
+}
